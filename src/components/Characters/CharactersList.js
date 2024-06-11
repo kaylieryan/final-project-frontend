6faad3a897d035c8299d60149758e199128bd06c
kaylieryan <kayliemryan@gmail.com>
2024-06-11 08:48:50 -0700

    not use axios

--- src/components/Characters/CharactersList.js
+++ src/components/Characters/CharactersList.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import './CharactersList.css';
 
 function CharactersList() {
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,24 +1,16 @@
-import axios from "axios";
 import { baseURL } from "constants.js";
 
 //GET Characters
-
 export const fetchCharacters = () => {
-  const getCharacters = axios
-    .get(`${baseURL}/characters`, {
+  const getCharacters = fetch(`${baseURL}/characters`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
-    })
-    .then((response) => {
-      // Access the 'data' property of the response object
-      const charactersData = response.data.data; // Adjusted to access the nested 'data'
-      return charactersData;
-    })
-    .catch((error) => {
-      throw error;
-    });
-
+    }).then((processServerResponse) => {
+    
   return getCharacters;
-};
+});
+}
+
+
