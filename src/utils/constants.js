262cb0351db134a31e444f3512a1b4e24095efbc
kaylieryan <kayliemryan@gmail.com>
2024-06-11 08:48:33 -0700

    create server response

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,9 @@
 export const baseURL = 'https://api.potterdb.com/v1'; // Base URL for the API
+
+export const processServerResponse = (res) => {
+  if (res.ok) {
+    return res.json();
+  } else {
+    return Promise.reject(`error: ${res.status}`);
+  }
+};
