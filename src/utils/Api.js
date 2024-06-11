85f726893a183a86616229b5d4dd800828f1de34
kaylieryan <kayliemryan@gmail.com>
2024-06-11 15:28:33 -0700

    Simplify the fetch.

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,13 +3,9 @@ import { processServerResponse } from "./constants";
 
 //GET Characters
 export const fetchCharacters = () => {
-  const getCharacters = fetch(`${baseURL}/characters`, {
-    method: "GET",
-    mode: "no-cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then(processServerResponse);
-
+  const getCharacters = fetch(`${baseURL}/characters`)
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => console.error("Error:", error));
   return getCharacters;
 };
