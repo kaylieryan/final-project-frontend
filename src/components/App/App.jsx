b002c470718be35731b21511c4353b13fedaf5ce
kaylieryan <kayliemryan@gmail.com>
2024-09-15 06:59:21 -0700

    create simple footer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 //import React from "react";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 import "./App.css";
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <div className="App">
       <Header />
       {/* Other components */}
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+//import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Developed by Kaylie Ryan with TripleTen</p>
+      <p>2024</p>
+    </footer>
+  );
+};
+
+export default Footer;
